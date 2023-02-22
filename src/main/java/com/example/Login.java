a79b3ac7736a1110d6d5608000eb08df31f51867
Baturay Arslan <baturay_arslan_fb@hotmail.com>
2023-02-22 19:25:26 +0300

    Login page implemented.

---
+++ src/main/java/com/example/Login.java
@@ -0,0 +1,108 @@
+package com.example;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtAddress;
+	private JTextField txtPort;
+
+	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setBounds(100, 100, 361, 444);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null);
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(79, 116, 187, 20);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblUsername.setBounds(81, 93, 91, 14);
+		contentPane.add(lblUsername);
+		
+		txtAddress = new JTextField();
+		txtAddress.setColumns(10);
+		txtAddress.setBounds(79, 173, 187, 20);
+		contentPane.add(txtAddress);
+		
+		JLabel lblAddress = new JLabel("Ip Address");
+		lblAddress.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblAddress.setBounds(81, 147, 91, 14);
+		contentPane.add(lblAddress);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(79, 231, 187, 20);
+		contentPane.add(txtPort);
+		
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPort.setBounds(81, 210, 91, 14);
+		contentPane.add(lblPort);
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				login();
+				
+			}
+		});
+		btnConnect.setBounds(128, 275, 89, 23);
+		contentPane.add(btnConnect);
+	}
+	
+	private void login() {
+		String userName = txtName.getText();
+		String address = txtAddress.getText();
+		int port = Integer.parseInt(txtPort.getText());
+		System.out.println("Username: " + userName + ", address: " + address +  ", port: " + port);
+		dispose();
+		
+	}
+
+	
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
